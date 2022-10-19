7181850c0f542cfbcbd2ab3a7b99742d92d5241a
fdymendo <fdyrepositorios@gmail.com>
2022-10-18 21:06:47 -0500

    Initial commit

---
+++ src/main/kotlin/com/fdymendo/javeriana/infracciones/InfraccionesApplication.kt
@@ -0,0 +1,11 @@
+package com.fdymendo.javeriana.infracciones
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InfraccionesApplication
+
+fun main(args: Array<String>) {
+	runApplication<InfraccionesApplication>(*args)
+}
