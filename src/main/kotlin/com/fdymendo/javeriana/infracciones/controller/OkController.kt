457b64eb1bbd5cc8fc74eccbf01018bff8225107
FdyMendo <fdymendo@users.noreply.github.com>
2022-10-28 19:42:31 -0500

    Develop (#13)

--- src/main/kotlin/com/fdymendo/javeriana/infracciones/controller/OkController.kt
+++ src/main/kotlin/com/fdymendo/javeriana/infracciones/controller/OkController.kt
@@ -1,15 +1,16 @@
 package com.fdymendo.javeriana.infracciones.controller
 
-import com.fdymendo.javeriana.infracciones.dto.InfractionDTO
-import com.fdymendo.javeriana.infracciones.service.IInfractionService
 import com.fdymendo.javeriana.infracciones.utils.GenericMethods
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 @RequestMapping("/infraccion")
 class OKController {
     @GetMapping
-    fun saveInfraction()  = GenericMethods.responseOk()
+    fun saveInfraction() = GenericMethods.responseOk()
 
 }
